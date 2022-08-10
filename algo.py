e62dc8fe259f14025bf3cb842b094359969fac35
Cole Dylewski <cole.dylewski@gmail.com>
2022-08-10 12:54:43 -0400

    8/10/2022

--- algo.py
+++ algo.py
@@ -1,22 +1,21 @@
 
 ##leetcode 704
 ##runtime 804ms
-## POTENTIAL BUG IF MID IS LARGER THAN
+
 def binary_search(arr, target):
     start = 0
     end = len(arr)-1
 
-    while start<=end:
-        mid = (start + end) // 2
+    while start<=end :
+        mid = start + ((end -start) // 2)
         val = arr[mid]
-        print(arr, target, start, mid, end)
+        print( target, start, mid, end)
         if target == val:
             return mid
         elif target>val:
                 start = mid+1
         else:
             end=mid-1
-
     return -1
 
 
